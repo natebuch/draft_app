9f976bc442d86092c45c3d1bba5073a33b0a3a55
nate buch <nate.buch@gmail.com>
2019-08-30 01:43:28 -0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -16,9 +16,11 @@ class App extends Component {
     return players.map(({player_name, overall_rank, positional_rank, bye_week}, index) => {
       return (
         <div key={ overall_rank }>
-          <li style={{ listStyle: "none" }}>{ overall_rank }. { player_name } Pos. { positional_rank } Bye: { bye_week } </li>  
-          <button onClick={ () => this.selectPlayer(index) }>Select</button>     
-        </div>
+          <li class="list-group-item">
+            { overall_rank }. { player_name } - Pos. { positional_rank } - Bye: { bye_week }
+            <button class="btn btn-warning btn-sm d-flex justify-content-right" onClick={ () => this.selectPlayer(index) }>Select</button>    
+          </li>
+       </div>
       )
     })
   }
@@ -41,9 +43,11 @@ class App extends Component {
     return players.map(({player_name, overall_rank, positional_rank, bye_week}, index) => {
       return (
         <div key={ overall_rank }>
-          <li style={{ listStyle: "none" }}>{ overall_rank }. { player_name } Pos. { positional_rank } Bye: { bye_week } </li>  
-          <button onClick={ () => this.unselectPlayer(index) }>Un-Select</button>     
-        </div>
+          <li class="list-group-item">
+            { overall_rank }. { player_name } - Pos. { positional_rank } - Bye: { bye_week }
+            <button class="btn btn-warning btn-sm d-flex justify-content-right" onClick={ () => this.unselectPlayer(index) }>Un-Select</button>    
+          </li>
+       </div>
       )
     })  
   }
@@ -63,19 +67,27 @@ class App extends Component {
 
   render() { 
     return (
-      <div style={{display: "flex", justifyContent: "flexStart"}}>
-        <section>
-          <h1>Available Players</h1>
-          <ul>
-            { this.listAvailablePlayers() }
-        </ul>
-        </section>
-        <section>
-          <h1>My Team</h1>
-          <ul>
-          { this.listUnavailablePlayers() }
-          </ul>
-        </section>
+      <div>
+        <div>
+          <nav className="navbar" style={{backgroundColor: '#900C3F'}}>
+            <img id="fork" src="https://i.redd.it/odmx4dqcaory.jpg" width="60" height="60" alt=""/>
+            <span id="logo" className="navbar-brand mb-0 h1" style={{color: "#FFC300"}}>S.D.I.F.F.L.</span>
+          </nav> 
+        </div> 
+        <div class="d-flex justify-content-center">
+          <section>
+            <h1 style={{color: "#900C3F"}}>Available Players</h1>
+            <ul class="list-group">
+              { this.listAvailablePlayers() }
+            </ul>
+          </section>
+          <section>
+            <h1 style={{color: "#900C3F"}}>My Team</h1>
+            <ul class="list-group">
+              { this.listUnavailablePlayers() }
+            </ul>
+          </section>
+        </div>
       </div>
     )
   }
